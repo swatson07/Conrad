362abbf487753c64a5ea55a9d3467aa016a91a99
Sam Watson <samjwatson07@gmail.com>
2024-01-31 21:15:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ print("Hello World!")
 coninput = input("I love Conrad!!!")
 if coninput == "hell yeah":
     print("Thank you for appreciating Conrad.")
-    
+else:
+    print("Your ignorance of the goat will not go unnoticed")
