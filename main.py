1b01582bf705e3cf0ae30bbab07830c354472bc7
jboam07 <158350363+jboam07@users.noreply.github.com>
2024-02-01 09:39:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ coninput = input("I love Conrad!!!")
 if coninput == "hell yeah":
     print("Thank you for appreciating Conrad.")
 else:
-    print("Your ignorance of the goat will not go unnoticed")
+    print("Your ignorance of the goat of the goats will not go unnoticed")
