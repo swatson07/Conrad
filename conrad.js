a830ad22dc73554ad5b4e01b498c62298c00f688
Sam Watson <122746416+swatson07@users.noreply.github.com>
2024-02-01 10:47:35 +0000

    conrad

---
+++ conrad.js
@@ -0,0 +1,3 @@
+function conrad() {
+    window.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiNeiWD16sQ_c4GWhaRWFLUmgc3v69EvRpSP7IztGQpddb2w1NtXbGrk-rwowRX_c0sE0:https://media.licdn.com/dms/image/D4E03AQGnS-Wnouo8_g/profile-displayphoto-shrink_800_800/0/1695384293182%3Fe%3D2147483647%26v%3Dbeta%26t%3DJL_p8YM-WSzx9ZJVd0rY_UhfxauR1HrINAGOQe_aXRA&usqp=CAU")
+}
